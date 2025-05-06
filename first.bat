f2e4d0649c00d5171e4aa76672e4df209b523a38
Chamikara Madushanak <171785216+chamikara12@users.noreply.github.com>
2025-05-06 12:27:30 +0530

    Basic codes

---
+++ first.bat
@@ -0,0 +1,33 @@
+mkdir criteria_1
+cd criteria_1
+mkdir standard_1
+mkdir standard_2
+cd..
+mkdir criteria_2
+cd criteria_2
+mkdir standard_1
+mkdir standard_2
+cd..
+mkdir criteria_3
+cd criteria_3
+mkdir standard_1
+mkdir standard_2
+mkdir standard_3
+cd..
+mkdir criteria_4
+cd criteria_4
+mkdir standard_1
+mkdir standard_2
+mkdir standard_3
+cd..
+mkdir criteria_5
+cd criteria_5
+mkdir standard_1
+mkdir standard_2
+mkdir standard_3
+cd..
+mkdir criteria_6
+cd criteria_6
+mkdir standard_1
+mkdir standard_2
+mkdir standard_3
